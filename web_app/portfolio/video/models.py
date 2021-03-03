4d298d08f9f15c1429b37a32e080f1b6dd10148a
Rockerkoonju <78637372+Rockerkoonju@users.noreply.github.com>
2021-03-03 08:16:33 +0700

    Update models.py

--- web_app/portfolio/video/models.py
+++ web_app/portfolio/video/models.py
@@ -1,6 +1,24 @@
 from django.db import models
 
-class Video(models.Model):
-    title = models.CharField(max_length=50, unique=True)
-    episode = models.CharField()
+class Channels(models.Model):
+    name = models.CharField(max_length=20)
+    
+    def __str__(self):
+        retrun self.name
 
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+    
+    def __str__(self):
+        retrun self.name
+        
+class Video(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50, unique=True)
+    description = models.TextField()
+    key = models.CharField(max_length=100, unique=True)
+    channel = models.ManyToManyField('Channels', related_name='Video')
+    categories = models.ManyToManyField('Category', related_name='Video')
+    
+    def __str__(self):
+        return self.title + " | " + self.id
